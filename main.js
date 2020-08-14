adb9dbe13f4fdf2af2cf673a93256e886deb6a73
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-08-14 16:20:42 -0400

    layers test

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var mymap = L.map('map', {
     center: [42.3612, -71.0669],
-    zoom: 15,
-  });
+    zoom: 13,
+});
+
+var controlLayers = L.control.layers( null, null, {
+  position: "topright",
+  collapsed: false
+}).addTo(map);
 
 var norm = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -10,17 +15,15 @@ var norm = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiaGFydmFyZG1hcHMiLCJhIjoiY2tkMG91YjBvMGQ3YjJxcDB2a3l2M3lvOCJ9.PDmYUq9MgJwq3JlWAlg3wA'
-}).addTo(mymap);
+});
+controlLayers.addBaseLayer(norm, 'OSM basemap');
 
 var light = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
-});
+}).addTo(mymap);
+controlLayers.addBaseLayer(light, 'Carto Light basemap');
 
 var terrain = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
 });
-
-var controlLayers = L.control.layers( null, null, {
-  position: "topright",
-  collapsed: false
- }).addTo(map);
+controlLayers.addBaseLayer(terrain, 'Stamen Terrain basemap');

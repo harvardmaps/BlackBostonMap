025ec595bc68f55296e703310f9f515fe3cfc592
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-09-23 10:35:03 -0400

    testing

--- main.js
+++ main.js
@@ -3,19 +3,60 @@ var mymap = L.map('map', {
       zoom: 15,
   });
   
-  var controlLayers = L.control.layers( null, null, {
-    position: "topright",
-    collapsed: false
-  }).addTo(mymap);
-  
-  var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+ var geodata =  {
+    "type": "FeatureCollection",
+    "features": [{
+			"type": "Feature",
+			"properties": {
+				"NAME_1": "Hayden",
+				"NAME_2": "Harriet",
+				"DESC_OCC": null,
+				"RACE": "Black",
+				"TYPE": "Person",
+				"ACTION": "lived",
+				"START_LOC": 1850.0,
+				"END_LOC": 1889.0,
+				"LAT": 42.360223,
+				"LONG": -71.069049,
+				"ORIG_ADDRESS": "66 Southac St",
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-71.069049, 42.360223]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"NAME_1": "Henry",
+				"NAME_2": "John",
+				"DESC_OCC": "mariner",
+				"RACE": "Black",
+				"TYPE": "Person",
+				"ACTION": "lived",
+				"START_LOC": 1837.0,
+				"END_LOC": 1865.0,
+				"LAT": 42.36049,
+				"LONG": -71.06505,
+				"ORIG_ADDRESS": null,
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-71.06505, 42.36049]
+			}
+    }
+  ]
+}
+
+L.geoJson(geodata).addTo(mymap);
+
+
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
      maxZoom: 17,
      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
   
-  controlLayers.addBaseLayer(OpenTopoMap, 'Open Topo Map');
-  
-  var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
      subdomains: 'abcd',
      minZoom: 1,
@@ -23,25 +64,31 @@ var mymap = L.map('map', {
      ext: 'jpg'
   });
   
-  controlLayers.addBaseLayer(Stamen_Watercolor, 'Stamen Watercolor basemap');
-  
-  var Sanborn_1867 = L.tileLayer(
-      'https://s3.us-east-2.wasabisys.com/urbanatlases/39999059012052/tiles/{z}/{x}/{-y}.png', {
-          tms: true, 
-          attribution: 'Leventhal Map & Education Center'
+var Sanborn_1867 = L.tileLayer(
+    'https://s3.us-east-2.wasabisys.com/urbanatlases/39999059012052/tiles/{z}/{x}/{-y}.png', {
+        tms: true, 
+        attribution: 'Leventhal Map & Education Center'
       }
-  ).addTo(mymap);
-  
-  controlLayers.addBaseLayer(Sanborn_1867, '1867 Sanborn');
+  )
 
-  var Beers_1874 = L.tileLayer(
-	'https://s3.us-east-2.wasabisys.com/urbanatlases/39999059015410/tiles/{z}/{x}/{-y}.png', {
-		tms: true, 
+var Beers_1874 = L.tileLayer(
+  'https://s3.us-east-2.wasabisys.com/urbanatlases/39999059015410/tiles/{z}/{x}/{-y}.png', {
+	  tms: true, 
 		attribution: 'Leventhal Map & Education Center'
 	}
 );
 
-controlLayers.addBaseLayer(Beers_1874, '1874 F.W. Beers & Co');
+var basemaps = {
+  "Stamen Watercolor" : Stamen_Watercolor,
+  "Open Topo Map" : OpenTopoMap
+};
+
+var overlays = {
+	"Beers, 1874" : Beers_1874,
+	"Sanborn, 1867" : Sanborn_1867
+	};
+
+L.control.layers(basemaps, overlays).addTo(mymap);
 
 /*
 var latlng = (42.3554, -71.0762)

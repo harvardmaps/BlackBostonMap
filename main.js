aef287b826e0094efe17487af52bff2709f9a7ce
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-11-16 11:28:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ $( "#reset-button" ).click(function() {
 });
 */
 
-$('#controls').on('click', 'reset-button', function(){
+$('#controls').on('click', '.reset-button', function(){
 	sidebar.setContent('<h1>Sidebar for popup content</h1>' + 
 	'<div id="controls"><button id="reset-button">Reset</button></div>' +
   	'<h2>Click on a place to learn more</h2>')

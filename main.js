9e6b044889dd0a68e4663fa0ee98c9888333cef9
harvardmaps <68121177+harvardmaps@users.noreply.github.com>
2020-11-16 10:49:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -114,7 +114,7 @@ function buildLabel(feature) {
 */
 
 // Use $( "elementID") and the jQuery click listener method to remove on the remove button
-$( "#reset-button" ).prev("a.ui-btn").unbind().click( function()  {
+$(document).on('click', '#reset-button', function()  {
 	sidebar.setContent('<h1>Sidebar for popup content</h1>' + 
   '<div id="controls"><button id="reset-button">Reset</button></div>' + 
   '<h2>Click on a place to learn more</h2>')
